9da4d4e8e25791166a3478a273f9e7be05854c76
21au <audreypramudita@gmail.com>
2023-11-02 08:58:23 +0700

    Second Changes

--- app/src/test/java/com/example/audrey/junitTest.kt
+++ app/src/test/java/com/example/audrey/junitTest.kt
@@ -5,10 +5,11 @@ import org.junit.Assert
 import org.junit.Test
 
 class junitTest {
-    @Test
-    fun test() {
-        val audrey = junit()
-        val result = audrey.tambah(3,3)
-        Assert.assertEquals("Berhasil",6,result)
+    @Test //ini itu untuk menandai kalau metode yang mengikuti anotasi ini itu metode pengujian
+    fun test() { //Inisiasi metode pengujian, nama metodenya itu test
+        val audrey = junit() //membuat objek audrey lalu memanggil junit
+        val result = audrey.tambah(3,3) //memanggil tambah lalu diberi 2 argumen yaitu 3 dan 3
+        Assert.assertEquals("Berhasil",6,result) //hasil uji menggunakan method assert yang disediakan
+        //oleh jUnit tampilan akhirnya yaitu pesan Berhasil angka 6 sebagai hasil, result itu nilai yang diuji
     }
 }
